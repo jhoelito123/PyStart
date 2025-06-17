35736a2427f557efc6f5029d42f7f4760bf0ec85
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-06-17 01:49:03 -0400

    feat: update show data curso

--- Frontend/src/features/courses/pages/show-course.tsx
+++ Frontend/src/features/courses/pages/show-course.tsx
@@ -6,16 +6,17 @@ import { useParams } from 'react-router';
 type Curso = {
   id_curso: number;
   nombre_curso: string;
-  calificacion_curso: number;
   duracion_curso: string;
   descripcion_curso: string;
   portada_curso: string;
   fecha_inicio_curso: string;
   fecha_cierre_curso: string;
   profesor_curso: number;
-  modulo_curso: number;
-  idioma_curso: number;
-  dificultad_curso: number;
+  modulo: number;
+  idioma: number;
+  dificultad: string;
+  profesor: string;
+  //secciones: Array
 };
 
 export default function ShowCoursePage() {
@@ -24,7 +25,7 @@ export default function ShowCoursePage() {
     data: curso,
     loading,
     error,
-  } = useFetchData<Curso>(`${API_URL}/education/curso/${id}`);
+  } = useFetchData<Curso>(`${API_URL}/education/cursos/${id}`);
 
   if (loading) return <p className="p-4">Cargando curso...</p>;
   if (error || !curso)
@@ -35,13 +36,13 @@ export default function ShowCoursePage() {
       <CardShowCourse
         title={curso.nombre_curso}
         university="Universidad Andina"
-        language="Español"
-        level={mapDificultad(curso.dificultad_curso)}
+        language={curso.idioma}
+        level={curso.dificultad}
         imageUrl={curso.portada_curso}
         description={curso.descripcion_curso}
-        duration={curso.duracion_curso}
-        practices={2} // Puedes ajustar esto si lo tienes en la API
-        quizzes={3} // Puedes ajustar esto si lo tienes en la API
+        duration={convertirDuracionAHoras(curso.duracion_curso).toFixed(1)}
+        practices={2}
+        quizzes={3}
         syllabus={['Sección 1', 'Sección 2', 'Sección 3']}
         tabs={{
           general: curso.descripcion_curso,
@@ -53,15 +54,7 @@ export default function ShowCoursePage() {
   );
 }
 
-function mapDificultad(dificultad: number): string {
-  switch (dificultad) {
-    case 1:
-      return 'Básico';
-    case 2:
-      return 'Intermedio';
-    case 3:
-      return 'Avanzado';
-    default:
-      return 'Desconocido';
-  }
+function convertirDuracionAHoras(duracion: string): number {
+  const [horas, minutos, segundos] = duracion.split(':').map(Number);
+  return horas + minutos / 60 + segundos / 3600;
 }
