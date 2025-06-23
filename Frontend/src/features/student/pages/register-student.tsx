e920fad2d105afde917acd18c1af4da328b28dce
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-06-23 16:05:59 -0400

    feat: create component form and page for register student

---
+++ Frontend/src/features/student/pages/register-student.tsx
@@ -0,0 +1,9 @@
+import { FormRegisterStudent } from '../components/form-register-student';
+
+export default function RegisterStudentPage() {
+  return (
+    <div className="h-full w-full flex flex-col items-center justify-center">
+      <FormRegisterStudent />
+    </div>
+  );
+}
