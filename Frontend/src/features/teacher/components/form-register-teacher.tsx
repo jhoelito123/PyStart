3524f0cfaf2d4facd38dfa4e3f75cbd43b0a80eb
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-06-24 11:08:03 -0400

    feat: update teacher registration form with new fields and validation rules 🎉✏️

--- Frontend/src/features/teacher/components/form-register-teacher.tsx
+++ Frontend/src/features/teacher/components/form-register-teacher.tsx
@@ -1,103 +1,105 @@
-import { useForm } from 'react-hook-form';
 import { InputText } from '../../../components/ui/input';
 import { Button } from '../../../components/ui/button';
-
-interface TeacherFormData {
-  nombres: string;
-  apellidos: string;
-  cedula: string;
-  fechaNacimiento: string;
-  correo: string;
-  celular: string;
-}
+import { useTeacherForm } from '../hooks/use-teacher';
 
 export const FormRegisterTeacher = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<TeacherFormData>({
-    mode: 'onChange',
-    defaultValues: {},
-  });
-
-  const onSubmit = (data: TeacherFormData) => {
-    console.log(data);
-  };
+    isSubmitting,
+    validationRules,
+    onSubmit,
+    formState: { errors, isValid },
+  } = useTeacherForm();
 
   return (
     <div className="flex flex-col w-10/12 max-w-screen h-full my-10">
-      <div className="w-full h-6 rounded-t-2xl bg-blue-500" />
+      <div className="w-full h-6 rounded-t-2xl bg-blue-500" />{' '}
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="w-full justify-center shadow-2xl rounded-2xl p-10 bg-white mx-auto"
       >
         <div className="flex flex-row w-full max-w-11/12 mx-auto gap-32">
-          <div className="flex flex-col gap-5 items-center justify-center w-full">
+          <div className="flex flex-col gap-5 w-full">
             <div className="w-full">
               <h1 className="text-left text-slate-800 headline-lg">
                 Registrar Docente
               </h1>
-            </div>
+            </div>{' '}
             <div className="flex flex-row w-full gap-20">
               <InputText
                 label="Nombres"
-                name="nombres"
+                name="nombre_docente"
                 className="w-full"
                 register={register}
-                validationRules={{ required: 'El nombre es obligatorio' }}
+                validationRules={validationRules.nombre_docente}
                 errors={errors}
               />
               <InputText
                 label="Apellidos"
-                name="apellidos"
+                name="apellidos_docente"
                 className="w-full"
                 register={register}
-                validationRules={{ required: 'El apellido es obligatorio' }}
+                validationRules={validationRules.apellidos_docente}
                 errors={errors}
               />
             </div>
             <div className="flex flex-row w-full gap-20">
               <InputText
                 label="Cédula de Identidad"
-                name="cedula"
+                name="ci_docente"
                 className="w-full"
                 register={register}
-                validationRules={{ required: 'La cédula es obligatoria' }}
+                validationRules={validationRules.ci_docente}
                 errors={errors}
               />
               <InputText
-                label="Fecha de nacimiento"
-                name="fechaNacimiento"
-                type="date"
+                label="Número de celular"
+                name="telefono_docente"
                 className="w-full"
                 register={register}
-                validationRules={{ required: 'La fecha es obligatoria' }}
+                validationRules={validationRules.telefono_docente}
+                errors={errors}
+              />
+            </div>
+            <div className="flex flex-row gap-20 w-full">
+              <InputText
+                label="Nombre de usuario"
+                name="user.username_user"
+                className="w-full"
+                register={register}
+                validationRules={validationRules['user.username_user']}
+                errors={errors}
+              />
+              <InputText
+                label="Contraseña"
+                name="user.password_user"
+                type="password"
+                className="w-full"
+                register={register}
+                validationRules={validationRules['user.password_user']}
                 errors={errors}
               />
             </div>
             <div className="flex flex-row w-full gap-20">
               <InputText
-                label="Correo electrónico institucional"
-                name="correo"
+                label="Correo electrónico"
+                name="user.email_user"
+                type="email"
                 className="w-full"
                 register={register}
-                validationRules={{ required: 'El correo es obligatorio' }}
-                errors={errors}
-                labelPadding="whitespace-nowrap"
-              />
-              <InputText
-                label="Número de celular"
-                name="celular"
-                className="w-full"
-                register={register}
-                validationRules={{ required: 'El número es obligatorio' }}
+                validationRules={validationRules['user.email_user']}
                 errors={errors}
               />
             </div>
             <div className="flex flex-row w-full justify-between mt-4">
               <Button label="Cancelar" variantColor="variant2" type="button" />
-              <Button label="Registrar" variantColor="variant1" type="submit" />
+              <Button
+                label={isSubmitting ? 'Registrando...' : 'Registrar'}
+                variantColor={isSubmitting ? 'variantDesactivate' : 'variant1'}
+                type="submit"
+                disabled={!isValid || isSubmitting}
+              />
             </div>
           </div>
           <div className="flex flex-col items-center justify-center w-7/12">
